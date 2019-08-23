cff1926cccc70c5547b0fc7ed6594b8bf026049d
Evgeny <zhenya.2015aa@gmail.com>
2019-08-24 00:04:00 +0300

    version 1.6

--- src/by/epam/arraysOfArrays/task1/Main.java
+++ src/by/epam/arraysOfArrays/task1/Main.java
@@ -11,19 +11,20 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter size matrix");
         int size = scn.nextInt();
+        System.out.println("Enter size matrix2");
         int size2 = scn.nextInt();
 
         int[][] array = new int[size][size2];
 
         for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array.length; j++){
+            for (int j = 0; j < array[0].length; j++){
                 array[i][j] = scn.nextInt();
             }
         }
 
         System.out.println("Your matrix");
         for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array.length; j++){
+            for (int j = 0; j < array[0].length; j++){
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
@@ -33,7 +34,7 @@ public class Main {
         for (int i = 0; i < array.length; i++){
             for (int j = 0; j < array.length; j++){
                 if (((j + 1) % 2 != 0) && (array[0][j] > array[array.length - 1][j])){
-                    System.out.println(array[i][j] + " ");
+                    System.out.print(array[i][j] + " ");
                 }
             }
             System.out.println();
--- src/by/epam/method/task8/Main.java
+++ src/by/epam/method/task8/Main.java
@@ -1,3 +1,4 @@
+package by.epam.method.task8;
 
 /*Задан массив D. Определить следующие суммы: D[l] + D[2] + D[3]; D[3] + D[4] + D[5]; D[4] +D[5] +D[6].
 Пояснение. Составить метод(методы) для вычисления суммы трех последовательно расположенных элементов
@@ -6,42 +7,34 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
 
-        System.out.println("Размер массива");
-        int size = scn.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of elements in the array");
+		int number = scanner.nextInt();
+		int [] array = new int[number];
 
-        int [] array = new int[size];
+		System.out.println("Enter elements");
+		for (int i = 0; i < array.length; i++) {
+			array[i] = scanner.nextInt();
+		}
 
-        for (int i = 0; i < array.length; i++){
-            array[i] = (int)(Math.random() * 10) - 0;
-        }
-        System.out.println("Массив");
-        for (int num: array){
-            System.out.print(num + " ");
-        }
+		System.out.println("array = ");
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
 
-        Sum(array);
+		System.out.println("\nD[1] + D[2] + D[3] = " + getSum(1, 3, array) + "\nD[3] + D[4] + D[5] = "
+				+ getSum(3, 5, array) + "\nD[4] + D[5] + D[6] = " + getSum(4, 6, array));
+	}
 
-    }
+	private static int getSum(int k, int m, int[] array) {
+		int sum = 0;
+		for (int i = k - 1; i < m; i++) {
+			sum += array[i];
+		}
+		return sum;
+	}
 
-    public static void Sum(int [] arr){
-
-        int count = 0;
-        int sum = 0;
-        for (int i = 0; i < arr.length ; i ++){
-            sum = sum + arr[i];
-            count++;
-            if (count == 3){
-                System.out.println();
-                System.out.println("Sum " + sum );
-                count = 0;
-                break;
-            }
-
-        }
-
-
-    }
 }
